28700b7d6163363c80ca706fdcdb84f723385383
Anthony <gioant1@hotmail.com>
2023-07-02 15:33:03 -0400

    gave names to each custom color to avoid issues in future

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
       fontFamily: {
         'space-mono': ['"Space Mono"', 'monospace'],
       },
+      colors: {
+          blue: '#0077ff',
+          'broken-blue': '#265f7d',
+          'glaucous': '#697C9B',
+          'true-blue': '#4B699B',
+          'space-cadet': '#1E2B48',
+          'gunmetal': '#2A3341',
+          'oxford-blue': '141D2E',
+          white: '#ffffff',
+          'ghost-white': '#f5f7ff',
+      },
     },
   },
   plugins: [],
